0c28f27a4b676270f3aa7ca7e155a5b372ba071c
Rajeshnay <146355131+Rajeshnay@users.noreply.github.com>
2025-06-11 13:07:57 +0530

    Enable Swagger UI at /docs

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI
 from routes import order_routes
 
-app = FastAPI()
+app = FastAPI( 
+    title="Order Management API",
+    version="1.0",
+    docs_url="/docs",         # Swagger UI
+    redoc_url="/redoc",       # ReDoc UI
+    openapi_url="/openapi.json"  # OpenAPI schema
+      )
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI working!"}
 
-app.include_router(order_routes)
+app.include_router(order_routes, prefix="/api", tags=["Orders"])
